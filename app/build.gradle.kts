0101e107d9749bd491e11c2696c4061b7857df6a
MyBook Hype AMD <ariedwisulistyo@gmail.com>
2026-04-15 15:18:29 +0700

    add: dependencies and plugin room database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -64,6 +66,8 @@ android {
 }
 
 dependencies {
+    implementation("androidx.room:room-runtime:2.7.0")
+    ksp("androidx.room:room-compiler:2.7.0")
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material3:material3")
